98c893f7bb96d9fac0262bf1f41ddef50c5aea0f
Jakub Malinowski <112685197+jmalinowski404@users.noreply.github.com>
2022-09-12 14:47:06 +0200

    Initial commit

---
+++ start.bat
@@ -0,0 +1,7 @@
+echo "# HTML-CSS-JavaScript" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/jmalinowski404/HTML-CSS-JavaScript.git
+git push -u origin main
